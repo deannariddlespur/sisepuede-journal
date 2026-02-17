4ac7280bf87f2d4944f7d63794b21abe661b4577
DeAnna Lynn Riddlespur <deannariddlespur@users.noreply.github.com>
2026-02-17 16:27:03 -0600

    Update models.py

--- entries/models.py
+++ entries/models.py
@@ -225,7 +225,10 @@ class MediaItem(models.Model):
 
 
 class AboutPage(models.Model):
+    """Single-row content for the About page. Staff can only edit."""
+
     content = models.TextField(blank=True)
+    image = models.ImageField(upload_to='about/', blank=True, null=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
@@ -233,4 +236,4 @@ class AboutPage(models.Model):
         verbose_name_plural = 'About page'
 
     def __str__(self):
-        return 'About page'
+        return 'About page''About page'
